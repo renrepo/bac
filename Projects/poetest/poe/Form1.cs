30c0a5e76f55eed079c7244cc4e52f0802eba7b2
Rene Wabnitz <rene.wabnitz@uni-wuppertal.de>
2017-04-11 16:47:09 +0200

    update

--- Projects/poetest/poe/Form1.cs
+++ Projects/poetest/poe/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace poe
 {
@@ -18,27 +19,24 @@ namespace poe
         }
 
 
-        bool v1 = false;
-        bool v2 = false;
         public void tb1_TextChanged(object sender, EventArgs e)
-
         {
-            bool v1 = true;
+            
         }
 
 
         public void tb2_TextChanged(object sender, EventArgs e)
         {
-            bool v2 = true;
+            btn1_div.Enabled = true;
+            btn2_mult.Enabled = true;
+            btn3_sub.Enabled = true;
+            btn4_add.Enabled = true;
         }
+
+        
 
         private void btn3_sub_Click(object sender, EventArgs e)
         {
-            if (v1 == true && v2 == true)
-            {
-                btn3_sub.Enabled = false;
-                btn3_sub.Update();
-            }
             tb_erg.Text = calc.sub(Convert.ToInt32(tb1.Text), Convert.ToInt32(tb2.Text)).ToString();
         }
 
@@ -61,14 +59,119 @@ namespace poe
         {
             tb_erg.Text = calc.div(Convert.ToInt32(tb1.Text), Convert.ToInt32(tb2.Text)).ToString();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            tb_erg.Text = "";
+            tb1.Text = "";
+            tb2.Text = "";
+
+        }
+
+        private void btn_fib_Click(object sender, EventArgs e)
+        {
+            if (!backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void btn_end_fib_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.CancelAsync();
+            }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int[] fib = new int[50];
+            int sum = 0;
+            fib[0] = 1;
+            fib[1] = 1;
+            for (int i = 2; i < 50; i++)
+            {
+                fib[i] = fib[i - 1] + fib[i - 2];
+               // tb_erg.Text = fib[i].ToString();
+               // tb_erg.Update();
+                Thread.Sleep(500);
+                backgroundWorker1.ReportProgress((i+1)*2);
+                sum = fib[i];
+
+
+
+                if (backgroundWorker1.CancellationPending)
+
+                {
+                    e.Cancel = true;
+                    backgroundWorker1.ReportProgress(0);
+                    return;
+                }
+            }
+
+            e.Result = sum;
+           
+
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            label4.Text = e.ProgressPercentage.ToString() + " %";
+            tb_erg.Text = "calculating";
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                label4.Text = "Process cancelled";
+            }
+
+            else if (e.Error != null)
+            {
+                label4.Text = e.Error.Message;
+            }
+
+            else
+            {
+                label4.Text = e.Result.ToString();
+                tb_erg.Text = e.Result.ToString();
+                
+            }
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+         
+        }
     }
+
+
+
+
+
+
 
 
     public class calc
     {
-        public static double mult(double a,double b)
+        public static double mult(double a, double b)
         {
-            return a * b;   
+            return a * b;
         }
 
         public static double Add(double a, double b)
@@ -79,7 +182,7 @@ namespace poe
         public static double sub(double a, double b)
 
         {
-            return a - b;       
+            return a - b;
         }
 
         public static double div(double a, double b)
