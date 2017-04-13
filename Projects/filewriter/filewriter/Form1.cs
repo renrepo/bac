e8942687312b952a038b17546453199f5c3f87c1
Rene Wabnitz <rene.wabnitz@uni-wuppertal.de>
2017-04-13 19:39:46 +0200

    update

--- Projects/filewriter/filewriter/Form1.cs
+++ Projects/filewriter/filewriter/Form1.cs
@@ -15,36 +15,70 @@ namespace filewriter
 {
     public partial class Form1 : Form
     {
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string var;
+        string var2;
+        List<string> liste = new List<string>();
+        string now = DateTime.Now.ToString("yyyy-MM-dd-HH-mm");
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        
+
         private void btn1_Click(object sender, EventArgs e)
         {
             //string path = Path.GetTempFileName();
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            List<string> liste = new List<string>();
-
-
 
             //   File.WriteAllLines(path + @"\test.txt", liste);
-            string now = DateTime.Now.ToString("yyyy-MM-dd-HH-mm");
-            for (int i = 0; i <= 1000; i++)
-            using (var tw = new StreamWriter(path +  @"\test_" + now + ".txt", true))
+            for (int i = 0; i <= 5000; i++)
             {
-                {
-                        //DateTime now = DateTime.Now;
-                        now = DateTime.Now.ToString("yyyy-MM-dd-HH-mm");
-                        string var = "Beitrag Nummer" + i.ToString("000");
-                        string var2 = "zusätzlich" + (2 * i).ToString("000");
-                        tw.WriteLine(var +"\t" + var2 + "\t" + now);
-                        liste.Add(var + var2);
-                        tw.Close();
-                        Thread.Sleep(1);
+                //DateTime now = DateTime.Now;
+                var = "Beitrag Nummer" + i.ToString("000");
+                var2 = "zusätzlich" + (2 * i).ToString("000");
+                liste.Add(var + "\t" + var2 + "\t" + now);
+                //Thread.Sleep(1);
 
+            }
+        }
+
+        private void btn_safe_Click(object sender, EventArgs e)
+        {
+            /*           using (var tw = new StreamWriter(path + @"\test_" + now + ".txt", true))
+                       {
+                           //tw.WriteLine(var + "\t" + var2 + "\t" + now);
+                           foreach (var item in liste)
+                           {
+                               tw.Write(item + Environment.NewLine);
+                           }
+                           tw.Close();
+                       }
+                       */
+            safer.safe(path,liste,now);
+        }
+    }
+
+
+
+
+
+
+
+
+    public class safer
+    {
+
+        public static void safe(string path, List<string> liste, string now)
+        {
+            using (var tw = new StreamWriter(path + @"\test_" + now + ".txt", true))
+            {
+                //tw.WriteLine(var + "\t" + var2 + "\t" + now);
+                foreach (var item in liste)
+                {
+                    tw.Write(item + Environment.NewLine);
                 }
+                tw.Close();
             }
         }
     }
