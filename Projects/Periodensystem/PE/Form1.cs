528739f1e59ed162c4f40d5b8b563b01b0baca1e
Rene Wabnitz <rene.wabnitz@uni-wuppertal.de>
2017-06-01 05:28:28 +0200

    Periodensystem

---
+++ Projects/Periodensystem/PE/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Na_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button159_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void He_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (btn.ForeColor == Color.Black)
+            {
+                btn.ForeColor = Color.Red;
+            }
+
+            else
+            {
+                btn.ForeColor = Color.Black;
+            }
+        }
+
+        private void H_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (btn.ForeColor ==  Color.Black )
+            {
+                btn.ForeColor = Color.Red;
+            }
+
+            else
+            {
+                btn.ForeColor = Color.Black;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button139_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button134_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Be_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (btn.ForeColor == Color.Black)
+            {
+                btn.ForeColor = Color.Red;
+            }
+
+            else
+            {
+                btn.ForeColor = Color.Black;
+            }
+        }
+    }
+}
